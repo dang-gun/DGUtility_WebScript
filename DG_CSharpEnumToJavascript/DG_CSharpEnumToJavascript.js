724625ff332ce1b463f96e22addd248b8eea11a4
서울 집(메인) <1dong8@hanmail.net>
2020-02-23 03:00:07 +0900

    Flag Enum 추가

---
+++ DG_CSharpEnumToJavascript/DG_CSharpEnumToJavascript.js
@@ -0,0 +1,40 @@
+﻿function Converter()
+{
+    var txtOri = $("#txtOri");
+    var txtOutput = $("#txtOutput");
+
+    var sOutput = "";
+
+    //시작 중괄호 자르기
+    var arrCut01 = txtOri.text().split("{");
+
+    //첫줄과 2번째줄 만들기
+    sOutput += arrCut01[0] + "{";
+
+    //마지막 중괄호 찾기 및 제거
+    var arrCut02 = arrCut01[1].split("}");
+
+    //내용물 자르기
+    var arrCut03 = arrCut02[0].split(",");
+
+    //내용물 변환
+    for (var i = 0; i < arrCut03.length; ++i)
+    {
+        
+        //슬러시 3개(///)제거
+        var sItem = arrCut03[i].replace(/\/\/\//gi, "");
+		sItem = sItem.replace(/=/gi, ":");
+		
+        //<summary>를 주석 시작으로 변환
+        sItem = sItem.replace("<summary>", "/**");
+        sItem = sItem.replace("</summary>", "*/");
+
+        //변환 결과 저장
+        sOutput += sItem + ",";
+    }
+
+    //마직막 중괄호 넣기
+    sOutput = sOutput + "}";
+
+    txtOutput.text(sOutput);
+}
---
+++ DG_FlagEnum/DG_FlagEnum.js
@@ -0,0 +1,46 @@
+﻿/** 열거형 맴버를 플래그로 처리하는 것을 돕는 유틸 */
+var DG_FlagEnum = {};
+
+/**
+ * 원본에 비교할 키가 들어있는지 확인한다.
+ * @param {string} value 원본
+ * @param {string} key 키
+ * @returns {boolean} 일치 여부
+ */
+DG_FlagEnum.HasFlag = function (value, key)
+{
+    return !!(value & key);
+};
+
+/**
+ * 원본에 키를 추가한다.
+ * @param {string} value 원본
+ * @param {string} key 키
+ * @returns {type} 완성된 결과
+ */
+DG_FlagEnum.Add = function (value, key)
+{
+    return value | key;
+};
+
+/**
+ * 원본에 키를 제거한다.
+ * @param {string} value 원본
+ * @param {string} key 키
+ * @returns {type} 완성된 결과
+ */
+DG_FlagEnum.Delete = function (value, key)
+{
+    return value & ~key;
+};
+
+/**
+ * 원본에서 키를 토클해줍니다.
+ * @param {string} value 원본
+ * @param {string} key 키
+ * @returns {type} 완성된 결과
+ */
+DG_FlagEnum.Toggle = function (value, key)
+{
+    return value ^key;
+};
