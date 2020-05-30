9d0fa2fd685dff68a7d89789d0d668f58d9a3187
서울 집(메인) <1dong8@hanmail.net>
2020-05-30 17:52:38 +0900

    DG_FlagEnum : Html 생성 기능 추가, 체크박스에서 값받는 기능 추가

--- DG_FlagEnum/DG_FlagEnum.js
+++ DG_FlagEnum/DG_FlagEnum.js
@@ -44,3 +44,76 @@ DG_FlagEnum.Toggle = function (value, key)
 {
     return value ^key;
 };
+
+
+/**
+ * Flag 타입을 Html로 변환합니다.
+ * @param {string} sItemHtml html생성시 항목으로 사용할 html
+ * @param {json} typeFlag 값으로 쓸 타입json
+ * @param {json} jsonTypeFlagName 이름으로 쓸 json
+ * @returns {string} 완성된 Html
+ */
+DG_FlagEnum.ToHtml = function (
+    sItemHtml
+    , typeFlag
+    , jsonTypeFlagName)
+{
+
+    var sReturn = "";
+
+    //플래그 키리스트
+    var arrFlagKey = Object.keys(typeFlag);
+
+    for (var i = 1; i < arrFlagKey.length; ++i)
+    {
+        //선택된 값
+        var itemFlagKey = arrFlagKey[i];
+        var itemFlag = typeFlag[itemFlagKey];
+        var itemFlagName = jsonTypeFlagName[itemFlagKey];
+
+        var sItemHtmlTemp = sItemHtml;
+        //값 넣기
+        sItemHtmlTemp = sItemHtmlTemp.replace(/{{value}}/g, itemFlag);
+        //이름 넣기
+        sItemHtmlTemp = sItemHtmlTemp.replace(/{{Name}}/g, itemFlagName);
+
+        //완성된 아이템 저장
+        sReturn += sItemHtmlTemp;
+    }
+
+    return sReturn;
+};
+
+DG_FlagEnum.GetCheckBoxData = function(sGroupName)
+{
+    //계산된 권한 값
+    var typeAuthReturn = 0;
+
+    //체크된 dom 개체 검색
+    var arrChk = document.querySelectorAll("input[name=" + sGroupName + "]:checked");
+
+    //선택된 값계산
+    for (var i = 0; i < arrChk.length; ++i)
+    {
+        var itemChk = arrChk[i];
+        //값받기
+        var val = itemChk.value;
+        if ((typeof val === "undefined")
+            || (val === ""))
+        {//값이 없거나
+            //비어있다.
+            val = 0;
+        }
+        else
+        {//값이 있다.
+            //숫자형으로 바꾼다.
+            val = Number(val);
+        }
+
+        //리턴용 flag에 넣기
+        typeAuthReturn |= val;
+    }
+
+
+    return typeAuthReturn;
+};
